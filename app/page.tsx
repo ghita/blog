19665673d93f5ca90b075988da0fc9e3d207b990
Gheorghe Marinca <gheorghe.marinca@gmail.com>
2024-06-10 17:45:05 +0300

    just includes only the name of the blogs files (MDs)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import fs from "fs";
+
+const getPostMetadata = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownFiles = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownFiles.map((file) => file.replace(".md", ""));
+  return slugs;
+}
+
 const HomePage = () => {
-  return <h1>Hello from NextJS !</h1>
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((slug) => 
+    <div>
+      <h2>{slug}</h2>
+    </div>
+  );
+
+  return <div>{postPreviews}</div>
 }
 
 export default HomePage;
