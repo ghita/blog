7a619f8a54cc2b190cb71c934eeb1f30044bce06
Gheorghe Marinca <gheorghe.marinca@gmail.com>
2024-06-10 18:13:38 +0300

    generate non formatted post content on the fly
    - from MD data

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,7 +1,20 @@
-const PostPage = () => {
+import fs from 'fs';
+
+const getPostContent = (slug: string) => {
+    const folder = "posts/";
+    const file = `${folder}${slug}.md`;
+    const content = fs.readFileSync(file, "utf8");
+    return content;
+};
+
+
+const PostPage = ({ params }: { params: { slug: string } }) => {
+    const slug = params.slug;
+    const content = getPostContent(slug);
     return (
      <p>
-        <h1>This is a post !</h1>
+        <h1>This is post for: {slug}</h1>
+        <p>{content}</p>
      </p>
     );
 };
