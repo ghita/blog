a30f09da21ca7c31fb29c05e830c6d9fc5d9ed18
Gheorghe Marinca <gheorghe.marinca@gmail.com>
2024-06-11 12:25:57 +0300

    extracted as component PostPreview

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
 
 import getPostMetadata from "@/components/getPostMetadata";
+import PostPreview from "@/components/PostPreview";
 import Link from "next/link";
 
 const HomePage = () => {
   const postMetadata = getPostMetadata();
   const postPreviews = postMetadata.map((post, index) => 
-    <div key={index}>
-      <Link href={`/posts/${post.slug}`}>
-        <h2>{post.title}</h2>
-      </Link>
-      <p>{post.subtitle}</p>
-      <p>{post.date}</p>
-    </div>
+    <PostPreview  key={post.slug} {...post} />
   );
 
   return <div>{postPreviews}</div>
