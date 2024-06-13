3be8757d569bb90d12d43f27709e46bbb766a3d1
Gheorghe Marinca <gheorghe.marinca@gmail.com>
2024-06-13 18:33:36 +0300

    production requires a base path for resources

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV === 'development';
+
 const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true
-    }
+    },
+    basePath: isDev ? '' : '/blog',
 };
 
 export default nextConfig;
